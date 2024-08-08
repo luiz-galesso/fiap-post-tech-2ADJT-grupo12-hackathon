f893ea847bd45a2967bb50f4358f5b2ec310e95f
Lucas Oliveira <lucasbjjaugusto@gmail.com>
2024-08-07 22:53:24 -0300

    ajuste no controller teste

--- ms-cliente/src/test/java/com/hackathon/fiap/mscliente/infrastructure/cliente/controller/ClienteControllerIT.java
+++ ms-cliente/src/test/java/com/hackathon/fiap/mscliente/infrastructure/cliente/controller/ClienteControllerIT.java
@@ -1,8 +1,10 @@
 package com.hackathon.fiap.mscliente.infrastructure.cliente.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hackathon.fiap.mscliente.infrastructure.cliente.controller.dto.ClienteRequestDTO;
 import com.hackathon.fiap.mscliente.usecase.cliente.CadastraCliente;
 import com.hackathon.fiap.mscliente.utils.ClienteHelper;
+import com.hackathon.fiap.mscliente.utils.GetAuth;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,14 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
 @AutoConfigureTestDatabase
 class ClienteControllerIT {
 
@@ -28,25 +28,29 @@ class ClienteControllerIT {
     @LocalServerPort
     private int port;
 
+    private String token;
+
     @BeforeEach
-    void setup() {
+    void setup() throws JsonProcessingException {
         RestAssured.port = port;
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        token = GetAuth.getToken();
     }
 
     @Test
-    @WithMockUser(username = "test", roles = {"admin"})
     void devePermitirCadastrarCliente() {
         ClienteRequestDTO clienteRequestDTO = ClienteHelper.gerarClienteRequest();
 
         given()
                 .filter(new AllureRestAssured())
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", "Bearer " + token)
                 .body(clienteRequestDTO)
                 .when()
                 .post("/api/cliente")
                 .then()
-                .log().all();
+                .log().all()
+                .statusCode(HttpStatus.OK.value());
     }
 
 }
